db2aa1f5384a014da8e96d4a4c74be29902d34a6
Cory Jbara <cjbara@nd.edu>
2016-11-07 12:36:24 -0500

    Initial Commit

---
+++ fiNDme/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  fiNDme
+//
+//  Created by Cory Jbara on 11/7/16.
+//  Copyright © 2016 coryjbara. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
