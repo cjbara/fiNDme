50f2475ff9798ab4a3cc7544f7104801364224bd
jsuarezm94 <jsuarezm@nd.edu>
2016-11-21 18:10:15 -0500

    Search, Find, Match

---
+++ fiNDme/SearchViewController.swift
@@ -0,0 +1,95 @@
+//
+//  SearchViewController.swift
+//  fiNDme
+//
+//  Created by Jose Alberto Suarez on 11/21/16.
+//  Copyright © 2016 coryjbara. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    enum TabIndex : Int {
+        case FirstChildTab = 0
+        case SecondChildTab = 1
+    }
+    
+    @IBOutlet weak var segmentedSearch: UISegmentedControl!
+    @IBOutlet weak var viewSearch: UIView!
+    
+    var currentViewController: UIViewController?
+    lazy var firstChildTabVC: UIViewController? = {
+        let firstChildTabVC = self.storyboard?.instantiateViewController(withIdentifier: "FindTableViewControllerId")
+        return firstChildTabVC
+    }()
+    lazy var secondChildTabVC : UIViewController? = {
+        let secondChildTabVC = self.storyboard?.instantiateViewController(withIdentifier: "MatchViewControllerId")
+        return secondChildTabVC
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //segmentedSearch.initUI()
+        //segmentedSearch.selectedSegmentIndex = TabIndex.FirstChildTab.rawValue
+        displayCurrentTab(tabIndex: TabIndex.FirstChildTab.rawValue)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let currentViewController = currentViewController {
+            currentViewController.viewWillDisappear(animated)
+        }
+    }
+    
+    // MARK: - Switching Tabs Functions
+    @IBAction func switchTabs(sender: UISegmentedControl) {
+        self.currentViewController!.view.removeFromSuperview()
+        self.currentViewController!.removeFromParentViewController()
+        
+        displayCurrentTab(tabIndex: sender.selectedSegmentIndex)
+    }
+    
+    func displayCurrentTab(tabIndex: Int){
+        if let vc = viewControllerForSelectedSegmentIndex(index: tabIndex) {
+            
+            self.addChildViewController(vc)
+            vc.didMove(toParentViewController: self)
+            
+            vc.view.frame = self.viewSearch.bounds
+            self.viewSearch.addSubview(vc.view)
+            self.currentViewController = vc
+        }
+    }
+    
+    func viewControllerForSelectedSegmentIndex(index: Int) -> UIViewController? {
+        var vc: UIViewController?
+        switch index {
+        case TabIndex.FirstChildTab.rawValue :
+            vc = firstChildTabVC
+        case TabIndex.SecondChildTab.rawValue :
+            vc = secondChildTabVC
+        default:
+            return nil
+        }
+        
+        return vc
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
